0fc12c0d900a4948b4062b9689c0277c0ba56a47
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-06-01 18:05:43 +0530

    RMX1851: Init from RMX1971
    - Replaced proprietary blobs with RMX1851
    - Imported extract-files.sh and setup-makefiles.sh from https://github.com/PixelExperience-Devices/device_realme_RMX1851

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1971.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_RMX1971-user \
-    lineage_RMX1971-userdebug \
-    lineage_RMX1971-eng
+    $(LOCAL_DIR)/lineage_RMX1851.mk
--- lineage_RMX1851.mk
+++ lineage_RMX1851.mk
@@ -7,17 +7,17 @@
 # Inherit some common Ricedroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from RMX1971 device.
+# Inherit from RMX1851 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Boot Animation
 SUSHI_BOOTANIMATION := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := lineage_RMX1971
+PRODUCT_DEVICE := RMX1851
+PRODUCT_NAME := lineage_RMX1851
 PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme 5 Pro
+PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
@@ -26,13 +26,13 @@ TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20191220.123812 release-keys"
+    PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20190909.050401 release-keys"
 
 BUILD_FINGERPRINT := "Realme/RMX1971/RMX1971:9/PKQ1.190101.001/1576816457:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="RMX1971" \
-    TARGET_DEVICE="RMX1971"
+    PRODUCT_NAME="RMX1851" \
+    TARGET_DEVICE="RMX1851"
 
 # Spoof build description/fingerprint as pixel device
 TARGET_USE_PIXEL_FINGERPRINT := true
@@ -40,13 +40,6 @@ TARGET_USE_PIXEL_FINGERPRINT := true
 # Pixel
 WITH_GMS := true
 TARGET_OPTOUT_GOOGLE_TELEPHONY := true
-
-# Package Type (By default it says AOSP,lets make it Vanilla instead)
-RICE_PACKAGE_TYPE := Vanilla
-
-# Official
-RICE_OFFICIAL := true
-RICE_MAINTAINER := KSSRAO
 
 # Graphene Camera
 TARGET_BUILD_GRAPHENEOS_CAMERA := false
