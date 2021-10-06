c1b4661b3bb3f28fdeae19f052e9b75e8e2809e6
Omar Hamad <etahamad@icloud.com>
2021-10-06 11:25:37 +0200

    RMX1851: Adapt sepolicy flags to Android 12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # Sepolicy
 include device/qcom/sepolicy/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
