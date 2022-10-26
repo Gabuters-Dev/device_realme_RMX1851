9a4bdcb27c63e273430814c6355d2386de2efb1e
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2022-10-27 01:48:57 +0700

    Nusantara Bringup!

--- nad_RMX1851.mk
+++ nad_RMX1851.mk
@@ -4,24 +4,23 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common Ricedroid stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Inherit from RMX1851 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Boot Animation
-SUSHI_BOOTANIMATION := 1080
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1851
-PRODUCT_NAME := lineage_RMX1851
+PRODUCT_NAME := nad_RMX1851
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
+
+NAD_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
@@ -33,19 +32,3 @@ BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.221005.002/9012097:user/release
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1851" \
     TARGET_DEVICE="RMX1851"
-
-# Spoof build description/fingerprint as pixel device
-TARGET_USE_PIXEL_FINGERPRINT := true
-
-# Pixel
-WITH_GMS := true
-TARGET_OPTOUT_GOOGLE_TELEPHONY := true
-
-# Graphene Camera
-TARGET_BUILD_GRAPHENEOS_CAMERA := false
-
-# AudioFx
-TARGET_EXCLUDES_AUDIOFX := true
-
-# Aperture Camera
-TARGET_BUILD_APERTURE_CAMERA := true
